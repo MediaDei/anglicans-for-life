2a885e8431c7e3fd6d97574ae80a97c521e7e012
Phasstw <phasstw@gmail.com>
2016-08-02 14:02:48 -0400

    More work on footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 		<footer>
 			<div class="grid">
 				<div class="grid-1-4">
-					
+					<p class="smallcaps logo">Anglicans</p>
+					<p class="smallcaps logo-bottom">for life</p>
 				</div>
 				<div class="grid-1-4">
 					<address>
@@ -9,6 +10,9 @@
 					</address>
 				</div>
 				<div class="grid-1-4">
+					<a class="contact" href="#">
+						<div class="icon fa fa-envelope-o"></div>
+					</a>
 					<script type="text/javascript">
 						var $ = jQuery.noConflict();
 						$(document).ready(function() {
@@ -27,33 +31,54 @@
 							var e = "(412)"; // area code
 							var t = "749"; // next 3 digits
 							var n = "0445"; // final 4 digits
-							var r = "tel:" + a + e + t + n; //href
+							var r = "tel:" + e + t + n; //href
 							$('#hidden-phone').attr('href',r).html(e + ' ' + t + ' ' + n);
 						});
 					</script>
 					<a id="hidden-phone" href="#">Please enable Javascript to view</a>
 				</div>
 				<div class="grid-1-4">
-					
+					<div class="icon-row">
+						<a href="#">
+							<div class="icon facebook fa fa-facebook"></div>
+						</a>
+						<a href="#">
+							<div class="icon twitter fa fa-twitter"></div>
+						</a>
+						<a href="#">
+							<div class="icon instagram fa fa-instagram"></div>
+						</a>
+					</div>
+					<a href="#">
+						<p class="terms-privacy">Terms and Privacy</p>
+					</a>
 				</div>
 			</div>
 			<div class="inset-prayer">
 				<h3 class="smallcaps">the prayer for life</h3>
-				<p>
-					Lord God,
-					Thank You for creating human life in Your image.
-					Thank You for my life and the lives of those I love.
-					Thank You for teaching us through Scripture the value You lace on life.
-					Help me to uphold the sanctity of life in my church and community.
-					Give me the strength to stand up to those forces
-					that seek to destroy the lives of those most vulnerable,
-					the unborn, the infirm and the elderly.
-					Today I commit myself never to be silent, never to be passive,
-					never to be forgetful of respecting life.
-					I commit myself to protecting and defending the sacredness of life
-					according to Your will, through Christ our Lord.
-					Amen
-				</p>
+				<div class="sentences">
+					<p>
+						Lord God, Thank You for creating human life in Your image.
+					</p>
+					<p>
+						Thank You for my life and the lives of those I love.
+					</p>
+					<p>
+						Thank You for teaching us through Scripture the value You place on life.
+					</p>
+					<p>
+						Help me to uphold the sanctity of life in my church and community.
+					</p>
+					<p>
+						Give me the strength to stand up to those forces that seek to destroy the lives of those most vulnerable, the unborn, the infirm and the elderly.
+					</p>
+					<p>
+						Today I commit myself never to be silent, never to be passive, never to be forgetful of respecting life.
+					</p>
+					<p>
+						I commit myself to protecting and defending the sacredness of life according to Your will, through Christ our Lord.
+					</p>
+				</div>
 				<h3 class="smallcaps">amen</h3>
 			</div>
 		</footer>
