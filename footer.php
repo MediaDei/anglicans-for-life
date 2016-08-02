45b1321ec2e14005d660987773dbf2943b1172bb
Phasstw <phasstw@gmail.com>
2016-08-01 22:38:26 -0400

    started footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,62 @@
-
+		<footer>
+			<div class="grid">
+				<div class="grid-1-4">
+					
+				</div>
+				<div class="grid-1-4">
+					<address>
+						405 Frederick Avenue<br>Sewickley, PA 15143
+					</address>
+				</div>
+				<div class="grid-1-4">
+					<script type="text/javascript">
+						var $ = jQuery.noConflict();
+						$(document).ready(function() {
+							var e = "info"; // replace with your email username
+							var t = "anglicansforlife"; // replace with your email provider
+							var n = ".com"; // replace with your email provider TLD
+							var r = "mailto:" + e + '@' + t + n; //href
+							$('#hidden-email').attr('href',r).html(e + '@'+t+n);
+						});
+					</script>
+					<a id="hidden-email" href="#">Please enable Javascript to view</a>
+					<script type="text/javascript">
+						var $ = jQuery.noConflict();
+						$(document).ready(function() {
+							var a = "1"; // extension
+							var e = "(412)"; // area code
+							var t = "749"; // next 3 digits
+							var n = "0445"; // final 4 digits
+							var r = "tel:" + a + e + t + n; //href
+							$('#hidden-phone').attr('href',r).html(e + ' ' + t + ' ' + n);
+						});
+					</script>
+					<a id="hidden-phone" href="#">Please enable Javascript to view</a>
+				</div>
+				<div class="grid-1-4">
+					
+				</div>
+			</div>
+			<div class="inset-prayer">
+				<h3 class="smallcaps">the prayer for life</h3>
+				<p>
+					Lord God,
+					Thank You for creating human life in Your image.
+					Thank You for my life and the lives of those I love.
+					Thank You for teaching us through Scripture the value You lace on life.
+					Help me to uphold the sanctity of life in my church and community.
+					Give me the strength to stand up to those forces
+					that seek to destroy the lives of those most vulnerable,
+					the unborn, the infirm and the elderly.
+					Today I commit myself never to be silent, never to be passive,
+					never to be forgetful of respecting life.
+					I commit myself to protecting and defending the sacredness of life
+					according to Your will, through Christ our Lord.
+					Amen
+				</p>
+				<h3 class="smallcaps">amen</h3>
+			</div>
+		</footer>
 	</div><!--close page-wrap opened in header.php-->
 </body>
 </html>
