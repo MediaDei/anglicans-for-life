4010823bc320c94b0f9d1217ade8b67288e2f851
Phasstw <phasstw@gmail.com>
2016-08-02 15:23:42 -0400

    started WP loop for page-home.php

--- page-home.php
+++ page-home.php
@@ -7,8 +7,41 @@ Template Name: Home Page
 <?php get_header(); ?>
 
 <section>
-	Home Page
-
+	<?php
+	$args = array(
+		'posts_per_page'	=> '3'
+	);
+	$query = new WP_Query($args);
+	if($query->have_posts()){
+		while($query->have_posts()){
+			$query->the_post();
+			?>
+			<article>
+				<time class="circle">
+					<div class="top"><?php echo get_the_date('M'); ?></div>
+					<div class="bottom"><?php echo get_the_date('d'); ?></div>
+				</time>
+				<?php if(has_post_thumbnail()){
+					?>
+					<figure><?php the_post_thumbnail(); ?></figure>
+					<?php
+				}
+				else{
+					?>
+					<figure>placeholder</figure>
+					<?php
+				}
+				?>
+				<div class="title-content">
+					<h1><?php the_title(); ?></h1>
+					<time class="pub-date"><?php echo 'Published ' . get_the_date('F j,Y'); ?></time>
+					<p><?php the_excerpt(); ?></p>
+				</div>
+			</article>
+			<?php
+		}
+	}
+	?>
 </section>
 
 <?php get_footer(); ?>
