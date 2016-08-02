f964385cf845b6ba9763914b50cb05f165c1c126
Phasstw <phasstw@gmail.com>
2016-08-02 17:51:55 -0400

    more work on page-home.php

--- page-home.php
+++ page-home.php
@@ -34,14 +34,48 @@ Template Name: Home Page
 				?>
 				<div class="title-content">
 					<h1><?php the_title(); ?></h1>
-					<time class="pub-date"><?php echo 'Published ' . get_the_date('F j,Y'); ?></time>
+					<time class="pub-date"><?php echo 'Published ' . get_the_date('F j, Y'); ?></time>
 					<p><?php the_excerpt('Read More'); ?></p>
 				</div>
 			</article>
 			<?php
 		}
+		?>
+		<a class="smallcaps older-posts" href="/archives">older posts &rarr;</a>
+		<?php
 	}
 	?>
+	<div class="donate">
+		<div class="content-wrapper">
+			<p>Partner with AFL’s life-affirming ministry today:</p>
+			<a class="button" href="">
+				<div class="button">Donate</div>
+			</a>
+		</div>
+	</div>
+	<div class="qa-twitter">
+		<div class="grid">
+			<div class="grid-2-3">
+				<h2>Ask:</h2>
+				<p class="question">
+					“There is a lot of recent pro-euthanasia legislation all over the country. I don’t think it’s right, but I don’t have a lot of answers either. Can you help?” -Anne
+				</p>
+				<a href="#" class="answer-link"> read & respond &rarr;</a>
+			</div>
+			<div class="grid-1-3">
+				<h2>Recent Tweets</h2>
+				<ul>
+					<li>Tweet</li>
+					<li>Tweet</li>
+					<li>Tweet</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="img-header-section">
+		<h2 class="smallcaps">get involved</h2>
+		<p>Lorem ipsum dolor sit amet arma virumque cano.</p>
+	</div>
 </section>
 
 <?php get_footer(); ?>
