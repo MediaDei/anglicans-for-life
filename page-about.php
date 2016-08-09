439b01f80ea6fadb8059a91be5cb95293a48a5a4
Phasstw <phasstw@gmail.com>
2016-08-09 12:48:19 -0400

    page-about.php static skel

---
+++ page-about.php
@@ -0,0 +1,157 @@
+<?php get_header(); ?>
+
+<section class="about">
+	<h1 class="page-title">Anglicans for Life</h1>
+	<p class="top-p">
+		<span class="simple-dropcaps">A</span><span class="smallcaps">s ministry based in</span> the Anglican tradition, AFL helps the Church apply the Biblical principles to the human Life issues that challenge people in local congregations and communities.
+	</p>
+	<h2>
+		<span class="italic">AFL 	Positions
+		</span>
+	</h2>
+	<div class="grid">
+		<div class="grid-1-2">
+			<div class="position-icon abortion">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Abortion
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon contraception">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Contraception
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon euthanasia">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Euthanasia
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon stem-cells">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Stem-cells
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon partial-birth-abortion">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Partial-birth abortion
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon post-abortion-aftercare">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Post-abortion aftercare
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon death-penalty">
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Death penalty
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+		<div class="grid-1-2">
+			<div class="position-icon adoption">	
+			</div>
+			<div class="text">
+				<h3>
+					<span class="italic">
+						Adoption
+					</span>
+				</h3>
+				<p>
+					Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien. 
+				</p>
+			</div>
+		</div>
+	</div>
+	<h3>
+		<span class="italic">
+			Our History
+		</span>
+	</h3>
+	<p class="history">
+		<span class="simple-dropcaps">V</span>
+		<span class="smallcaps">
+			orem ipsum dolor sit amet, 
+		</span>
+		consectetur adipisicing elit. Officiis asperiores quo, amet nihil magni ullam voluptate error perspiciatis ab deleniti itaque animi minima iste praesentium sequi eos sit quia aliquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste natus harum maiores aliquam veritatis aliquid odit minus dignissimos veniam, excepturi quibusdam minima dicta tempore beatae, animi quas, provident ipsa aperiam.
+	</p>
+	<p class="history">
+		<span class="smallcaps">lorem ipsum dolor sit amet,</span>
+		lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe ea qui tempore fuga, nemo architecto. Quo cupiditate expedita ipsam repudiandae ea voluptate quis velit sequi! Sapiente beatae ratione omnis sint. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum error rerum, sed beatae in! Sequi, tempora voluptate aut. Commodi sunt rerum, asperiores dolor quasi dolore nemo amet excepturi nobis necessitatibus?
+	</p>
+	<div class="bottom-history-row">
+		<p class="history-bottom">
+			<span class="simple-dropcaps">W</span>
+			<span class="smallcaps">e helped start</span>
+			the Silent No More Campaign.
+	    Vestibulum rutrum quam vitae fringilla tinci-dunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae.
+		</p>
+		<div class="gold-frame silent-img"></div>
+	</div>
+
+	<div class="our-staff new-section-bg">
+		
+	</div>
+</section>
+
+<?php get_footer(); ?>
