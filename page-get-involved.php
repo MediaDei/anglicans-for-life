1fef9af3a8f3edc02cb6fdda70065cf0d1767aa8
Phasstw <phasstw@gmail.com>
2016-08-10 14:41:30 -0400

    updates to page-get-involved.php

--- page-get-involved.php
+++ page-get-involved.php
@@ -2,17 +2,88 @@
 
 <section>
 	<h1 class="page-title">Get Involved</h1>
+
 	<p class="top-p">
-		Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien.
+		<span class="simple-dropcaps">M</span>
+		<span class="smallcaps">auris non tempor</span> quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est. Sed nec felis pellentesque, lacinia dui sed, ultricies sapien.
 	</p>
-	<a href="#priests-pastors-section" class="priests-pastors-link">Special Priests and Pastors section below &darr;</a>
+	<a href="#priests-pastors-section" class="priests-pastors-link">
+		Special section for Priests and Pastors below &darr;
+	</a>
+
 	<div class="pray-for-life">
 		<h2>Pray for life</h2>
-		<p><span class="italic">Join our prayer team:</span></p>
+		<p>
+			<span class="italic">Join our prayer team:</span>
+		</p>
 	</div>
+
+	<div class="grid">
+		<div class="grid-1-3">
+			<a class="bubble" href="#">
+				<div class="bubble  bubble-1"></div>
+			</a>
+			<h3 class="bubble-title">
+				Find a Life-Affirming Church or Leader
+			</h3>
+				<p class="bubble-text">
+					Arma virumque cano.
+			</p>
+		</div>
+
+		<div class="grid-1-3">
+			<a class="bubble" href="/lead-life-ministry/">
+				<div class="bubble bubble-2"></div>
+			</a>
+			<h3 class="bubble-title">
+				Lead Life Ministry
+			</h3>
+			<p class="bubble-text">
+				Help your church establish a life witness. Lorem ipsum sit.
+			</p>
+		</div>
+
+		<div class="grid-1-3">
+			<a class="bubble" href="/take-action/">
+				<div class="bubble  bubble-3"></div>
+			</a>
+			<h3 class="bubble-title">
+				Take Action for Life
+			</h3>
+			<p class="bubble-text">
+				Help people defend life and promote Christian values.
+			</p>
+		</div>
+	</div>
+
+	<div class="colored-section-break"></div>
 	<?php include(TEMPLATEPATH."/global-parts/subscribe-section-divider.php"); ?>
-	<?php include(TEMPLATEPATH."/global-parts/donate-section-divider.php"); ?>
+	<div class="colored-section-break"></div>
+
+	<div id="priests-pastors-section">
+		<h2>Priests & Pastors</h2>
+		<div class="grid lower-grid">
+			<div class="grid-1-2">
+				<a href="/pastoral-resources/" class="bubble">
+					<div class="bubble bubble-4">
+						
+					</div>
+				</a>
+				<h3 class="bubble-title">Pastoral Resources</h3>
+			</div>
+			<div class="grid-1-2">
+				<a href="/teaching-resources/" class="bubble">
+					<div class="bubble bubble-5">
+						
+					</div>
+				</a>
+				<h3 class="bubble-title">Teaching Resources</h3>
+			</div>
+		</div>
 	</div>
+
+
+	<?php include(TEMPLATEPATH."/global-parts/donate-section-divider.php"); ?>
 </section>
 
 <?php get_footer(); ?>
