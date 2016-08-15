39c3f4cddf457bf6a19617b49e220e104153469b
Phasstw <phasstw@gmail.com>
2016-08-14 22:50:27 -0400

    updates to page-lead-life-ministry.php/scss

--- page-lead-life-ministry.php
+++ page-lead-life-ministry.php
@@ -20,15 +20,106 @@
 		human life is described as sacred. We are made in God’s own image! But the culture around us celebrates death over life. The Church, therefore, needs to be zealous in its effort to celebrate, protect, and honor life. The Church needs people like you to step up to the challenge and begin life ministry today. Will you join us?
 	</p>
 	<div class="colored-section-break"></div>
-	
-	<?php //get page contents
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post(); 
-			the_content();
-		} // end while
-	} // end if
-	?>
+	<div class="leaders">
+		<h2>
+			Life Leaders
+			<div class="header-img life-leaders"></div>
+		</h2>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-1"></span>
+			<a href="#accordion-button-1" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">What is a Life Leader?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-1">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Vero quo, dolorum aspernatur harum ducimus veritatis illo eaque. Id natus quisquam repudiandae error rem voluptatibus est, rerum minima quis hic. Voluptate.
+				</p>
+			</div>
+		</div>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-2"></span>
+			<a href="#accordion-button-2" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">What does a Life Leader do?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-2">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Vero quo, dolorum aspernatur harum ducimus veritatis illo eaque. Id natus quisquam repudiandae error rem voluptatibus est, rerum minima quis hic. Voluptate.
+				</p>
+			</div>
+		</div>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-3"></span>
+			<a href="#accordion-button-3" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">I am interested. Where do I start?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-3">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Vero quo, dolorum aspernatur harum ducimus veritatis illo eaque. Id natus quisquam repudiandae error rem voluptatibus est, rerum minima quis hic. Voluptate.
+				</p>
+			</div>
+		</div>
+
+
+
+
+
+		<h2>
+			AFL Chapters
+			<div class="header-img chapter-leaders"></div>
+		</h2>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-4"></span>
+			<a href="#accordion-button-4" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">What is a Chapter?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-4">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Delectus velit maiores dolorem consectetur iste eligendi recusandae quae facere tempora harum nihil necessitatibus aspernatur atque, voluptatem architecto enim provident. Officiis, ipsam!
+				</p>
+			</div>
+		</div>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-5"></span>
+			<a href="#accordion-button-5" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">What does a Chapter Leader do?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-5">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Vero quo, dolorum aspernatur harum ducimus veritatis illo eaque. Id natus quisquam repudiandae error rem voluptatibus est, rerum minima quis hic. Voluptate.
+				</p>
+			</div>
+		</div>
+
+		<div class="accordion">
+			<span class="anchor" id="accordion-button-6"></span>
+			<a href="#accordion-button-6" class="accordion-button-anchor anchor-button">
+				<div class="accordion-button">
+					<p class="button-text">I am interested. Where do I start?</p>
+				</div>
+			</a>
+			<div class="hidden-content accordion-button-hidden accordion-button-6">
+				<p class="hidden-text hidden-button-content">
+					<span class="simple-dropcaps">L</span><span class="smallcaps">orem ipsum dolor sit amet,</span> consectetur adipisicing elit. Vero quo, dolorum aspernatur harum ducimus veritatis illo eaque. Id natus quisquam repudiandae error rem voluptatibus est, rerum minima quis hic. Voluptate.
+				</p>
+			</div>
+		</div>
+
+	</div>
 
 
 	<div class="colored-section-break"></div>
