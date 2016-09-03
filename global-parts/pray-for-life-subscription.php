0bd9e4bac814e503525b086091219e8fc8c8e142
Phasstw <phasstw@gmail.com>
2016-09-02 21:23:39 -0400

    added newsletter and prayer sub styles and functionality

--- global-parts/pray-for-life-subscription.php
+++ global-parts/pray-for-life-subscription.php
@@ -1,6 +1,5 @@
 <div class="pray-for-life">
 	<h2>Pray for life</h2>
-	<p>
-		<span class="italic">Join our prayer team:</span>
-	</p>
+	<p>Join our prayer team:</p>
+	<?php echo do_shortcode('[mc4wp_form id="251"]'); ?>
 </div>
--- global-parts/subscribe-section-divider.php
+++ global-parts/subscribe-section-divider.php
@@ -1,3 +1,4 @@
 <div class="subscribe">
 	<p>Sign up for <span class="italic">Carpe Diem</span>, AFL’s quarterly newsletter:</p>
+	<?php echo do_shortcode('[mc4wp_form id="196"]'); ?>
 </div>
