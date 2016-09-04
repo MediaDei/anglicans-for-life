9651719dc6245c8677febcba996f9a182ec38b49
Phasstw <phasstw@gmail.com>
2016-09-04 17:53:50 -0400

    Added Contact form styling

---
+++ page-contact.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section class="contact">
+	<h1 class="page-title">Contact</h1>
+	<span class="page-title-subtext">Anglicans for Life</span>
+	<?php echo do_shortcode('[contact-form-7 id="19" title="Contact Form"]'); ?>
+	<a href="/terms-privacy/">Please read our terms and privacy here</a>
+</section>
+
+<?php get_footer(); ?>
