beeafb39626cd58ef2c267b9567d6d1648c65e3a
Phasstw <phasstw@gmail.com>
2016-09-05 22:16:06 -0400

    fixed mailchimp form(s)

--- global-parts/pray-for-life-subscription.php
+++ global-parts/pray-for-life-subscription.php
@@ -1,5 +1,17 @@
 <div class="pray-for-life">
 	<h2>Pray for life</h2>
 	<p>Join our prayer team:</p>
-	<?php echo do_shortcode('[mc4wp_form id="251"]'); ?>
+	<form class="grid">
+		<script>
+			$(document).ready(function() {
+				var $ = jQuery.noConflict();
+				var $email = $("#mailchimp-email");
+				$("#pray-email").keyup(function() {
+				    $email.val( this.value );
+				});
+			});
+		</script>
+	  <input type="email" id="pray-email" name="pray-email" placeholder="Email Address"/>
+	  <a href="#subscribe" class="button"><div class="button">OK</div></a>
+	</form>
 </div>
--- global-parts/subscribe-section-divider.php
+++ global-parts/subscribe-section-divider.php
@@ -1,4 +1,21 @@
 <div class="subscribe">
 	<p>Sign up for <span class="italic">Carpe Diem</span>, AFL’s quarterly newsletter:</p>
-	<?php echo do_shortcode('[mc4wp_form id="196"]'); ?>
+	<form class="grid">
+	  <script>
+			$(document).ready(function() {
+				var $ = jQuery.noConflict();
+				var $email = $("#mailchimp-email");
+				$("#sub-email").keyup(function() {
+				    $email.val( this.value );
+				});
+			});
+		</script>
+	  <input type="email" id="sub-email" name="sub-email" placeholder="Email Address"/>
+	  <a href="#subscribe" class="button"><div class="button">Sign Up</div></a>
+	  <div class="social-buttons mailchimp">
+	  	<a class="icon" href="https://www.facebook.com/AnglicansforLife/"><div class="icon facebook fa fa-facebook"></div></a>
+			<a class="icon" href="https://twitter.com/Anglicans4Life"><div class="icon fa fa-twitter"></div></a>
+			<a class="icon" href="#"><div class="icon fa fa-pinterest"></div></a>
+	  </div>
+	</form>
 </div>
