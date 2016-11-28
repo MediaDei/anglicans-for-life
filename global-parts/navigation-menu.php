aa9f2c580d3fc226700a6721ddda0d99be3c3249
mediadei-james <mediadei-james@users.noreply.github.com>
2016-11-28 17:21:08 -0500

    updated link for AFL Canada

--- global-parts/navigation-menu.php
+++ global-parts/navigation-menu.php
@@ -4,6 +4,6 @@
 	<a class="link" href="http://vps8838.inmotionhosting.com/~anglicansforlife/shop/">Shop</a>
 	<a class="link" href="<?php echo get_site_url() ?>/blog/">Blog</a>
 	<a class="link" href="<?php echo get_site_url() ?>/about/">About Us</a>
-	<a class="link" href="http://vps8838.inmotionhosting.com/~anglicansforlife/canada/">Canada</a>
+	<a class="link" href="http://www.anglicansforlifecanada.com/">Canada</a>
 	<a class="link" href="http://vps8838.inmotionhosting.com/~anglicansforlife/archive/">Publications</a>
 </div>
--- media-dei-config-files/functions/media_dei_default_functions.php
+++ media-dei-config-files/functions/media_dei_default_functions.php
@@ -1,9 +1,5 @@
 <?php
 
-@ini_set( 'upload_max_size' , '64M' );
-@ini_set( 'post_max_size', '64M');
-@ini_set( 'max_execution_time', '300' );
-
 
 //allow Featured Images in Posts/Pages
 add_theme_support( 'post-thumbnails' );
